c077b87055c9dbcc1ecec2ef937fd4db1998d1dd
yadongqu <hejichong@126.com>
2016-11-09 13:23:12 +0100

    add ai part

--- game.js
+++ game.js
@@ -8,6 +8,7 @@ var Game = function() {
 	this.count = 0; // for winning rounds
 	this.turn = 0;  // 0 for human, 1 for ai;
 	this.winner = null;
+	this.cellValues = [];
 	this.init = function(){
 	  for(var i = 0; i < 6; i++)
 	  {
@@ -18,6 +19,15 @@ var Game = function() {
 			this.board[i][j] = 0;
 		}
 	  }
+
+	  this.cellValues =  [
+		[3, 4, 5, 7, 5, 4, 3],
+        [4, 6, 8, 10, 8, 6, 4],
+        [5, 8, 11, 13, 11, 8, 5],
+        [5, 8, 11, 13, 11, 8, 5],
+        [4, 6, 8, 10, 8, 6, 4],
+        [3, 4, 5, 7, 5, 4, 3]
+	  ];
 	  
 	};
 	this.start = function(){
@@ -34,13 +44,13 @@ var Game = function() {
 	}
 
 	// ai take random step for now.
-	this.getAIRandomMove = function(){
-		if(this.turn == 1){
-			var moves = this.getPossibleMove();
-			var move = Math.floor(Math.random() * moves.length);
-			return moves[move];
-		}
-	}
+	// this.getAIRandomMove = function(){
+	// 	if(this.turn == 1){
+	// 		var moves = this.getPossibleMove();
+	// 		var move = Math.floor(Math.random() * moves.length);
+	// 		return moves[move];
+	// 	}
+	// }
 
 	// get still open columns
 	this.getEmpty = function(){
@@ -80,9 +90,9 @@ var Game = function() {
 	this.getHumanMove = function(c){
 		var moves = this.getPossibleMove();
 		for(var i = 0; i < moves.length; i++){
-			console.log("checking " + moves[i]);
+			
 			if (moves[i][1] == c){
-				console.log("I choose " + moves[i]);
+				
 				return moves[i];
 			} 
 		}
@@ -95,11 +105,85 @@ var Game = function() {
 			}
 			
 			if(!this.getWinner() && this.turn == 1){
-				this.handleMove(this.getAIRandomMove(), AI);
+				this.handleMove(this.getAIMove(), AI);
 			}
 		    
 		}
 	}
+
+
+
+	this.is3InRow = function(move){
+		
+		if(this.getWinner() == AI){
+			this.cellValues[move[0][move[1]]] = 20000;
+		}
+
+		this.board[move[0]][move[1]] = HUMAN;
+		console.log("this is " + move);
+
+		if(this.getWinner() == HUMAN){
+		    console.log("human is winning");
+			this.cellValues[move[0]][move[1]] = 10000;
+			console.log(this.cellValues);
+		}
+		this.board[move[0]][move[1]] = 0;
+	}
+
+
+	this.is2InRow = function(){
+		var moves = this.getPossibleMove();
+		for(var i = 0; i < moves.length; i++){
+			for(var j = 0; j < moves.length; j++){
+
+				var move1 = moves[i];
+				var move2 = moves[j];
+				if(move1 != move2){
+					this.board[move1[0]][move1[1]] = HUMAN;
+					this.board[move2[0]][move2[1]] = HUMAN;
+					if(this.getWinner() == HUMAN){
+						if(Math.abs(move1[0] - move2[0]) == 3 || Math.abs(move1[1] - move2[1]) == 3){
+						   if(this.cellValues[move1[0]][move1[1]] < 5000){
+						   	this.cellValues[move1[0]][move1[1]] = 5000;
+						   }		
+							
+						}
+					}
+				}
+				this.board[move1[0]][move1[1]] = 0;
+				this.board[move2[0]][move2[1]] = 0;
+			}
+		}
+	}
+
+
+
+
+	this.getAIMove = function(){
+		var max = 0;
+		var bestMove = [];
+		var moves = this.getPossibleMove();
+		
+		for (var i = 0; i < moves.length; i++){
+			console.log("this is " + moves[i]);
+			var r = moves[i][0];
+			var c = moves[i][1];
+
+			this.is3InRow(moves[i]);
+			this.is2InRow();
+
+			
+			if(max < this.cellValues[r][c]){
+				max = this.cellValues[r][c];
+				bestMove = moves[i];
+			}
+		}
+		
+		return bestMove;
+	}
+
+
+
 
 	
 	this.handleMove = function(move, player){
